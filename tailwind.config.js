f5546c52a994fabc25772e030180823597228765
alex-voltagency <80480705+alex-voltagency@users.noreply.github.com>
2021-09-08 14:58:05 -0400

    Enter

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'backgroundc' : '#684BB1',
+      },
+      zIndex: {
+        '-10' : '-10',
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
